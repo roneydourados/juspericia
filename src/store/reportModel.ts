cbe1f7b4ae88144123d67489ecd9d5aeca02df5d
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2024-07-20 12:14:06 -0400

    store modelo laudo

---
+++ src/store/reportModel.ts
@@ -0,0 +1,62 @@
+import { ReportModelProps } from "@/types/Patient";
+import { defineStore } from "pinia";
+
+export const useReportModelStore = defineStore("reportModel", () => {
+  const { api } = useAxios();
+
+  const reportModel = ref<ReportModelProps>();
+  const reportModels = ref<ReportModelProps[]>([]);
+
+  const $all = computed(() => reportModels.value);
+  const $single = computed(() => reportModel.value);
+
+  const create = async (payload: ReportModelProps) => {
+    const { data } = await api.post<ReportModelProps>(
+      "/report-models",
+      payload
+    );
+
+    reportModel.value = data;
+  };
+
+  const index = async (inputQuery: string) => {
+    const config = {
+      params: {
+        inputQuery,
+      },
+    };
+
+    const { data } = await api.get<ReportModelProps[]>(
+      "/report-models",
+      config
+    );
+
+    reportModels.value = data;
+  };
+
+  const update = async (payload: ReportModelProps) => {
+    const { data } = await api.put<ReportModelProps>("/report-models", payload);
+
+    reportModel.value = data;
+  };
+
+  const destroy = async (id: number) => {
+    await api.delete(`/report-models/${id}`);
+  };
+
+  const show = async (id: number) => {
+    const { data } = await api.get<ReportModelProps>(`/report-models/${id}`);
+
+    reportModel.value = data;
+  };
+
+  return {
+    $all,
+    $single,
+    create,
+    index,
+    update,
+    destroy,
+    show,
+  };
+});
