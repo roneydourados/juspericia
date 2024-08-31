8800c568775c90a1cc9523d00fa1ec5a5300742d
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2024-08-30 20:19:19 -0400

    agenda

--- src/utils/FrontConstants.ts
+++ src/utils/FrontConstants.ts
@@ -16,3 +16,28 @@ export const solicitationStates = [
     value: "canceled",
   },
 ];
+
+export const daysOfWeek = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
+
+export const months: string[] = [
+  "Jan",
+  "Fev",
+  "Mar",
+  "Abr",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Set",
+  "Out",
+  "Nov",
+  "Dez",
+];
