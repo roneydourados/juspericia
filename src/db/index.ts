cb0fedf8a2c54ebd3f7519ce31a1ff4886078abf
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2024-09-14 11:27:33 -0400

    config inicial drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,8 +4,21 @@ import * as schema from "@/db/schema";
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "",
+// export const connection = new Pool({
+//   connectionString: process.env.DATABASE_URL || "",
+//   max: process.env.DB_MIGRATING || process.env.DB_SEEDING ? 1 : undefined,
+//   //onnotice: proce.env.DB_SEEDING ? () => {} : undefined,
+// });
+
+export const connection = new Pool({
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT as string),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  max: process.env.DB_MIGRATING || process.env.DB_SEEDING ? 1 : undefined,
 });
 
-export const db = drizzle(pool, { schema });
+export const db = drizzle(connection, { schema, logger: true });
+export type db = typeof db;
+export default db;
--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "drizzle-kit";
+import env from "./env";
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./db/schema/index.ts",
-  out: "./db/drizzle",
+  out: "./db/migrations",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "",
+    url: env.DATABASE_URL, //process.env.DATABASE_URL || "",
   },
   verbose: true,
   strict: true,
