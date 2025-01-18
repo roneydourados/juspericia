293854439210a3551ae151aca6f5c7c8162069c7
“Roney <roneylima@zdzcode.com.br>
2025-01-18 18:05:36 -0400

    remove log

--- src/lib/asaas/customer-api.ts
+++ src/lib/asaas/customer-api.ts
@@ -12,9 +12,6 @@ export const useCustomerAsaas = () => {
 
   const baseUrl = (useRuntimeConfig().public.asaasBaseUrl as string) || "";
 
-  console.log("🚀 ~ useCustomerAsaas ~ baseUrl:", baseUrl);
-  console.log("🚀 ~ useCustomerAsaas ~ apiKey:", apiKey);
-
   const asaasApi = axios.create({
     baseURL: baseUrl,
     headers: {
