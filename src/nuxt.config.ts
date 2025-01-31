d6d36c59b25eb42a535a31f4ea2263f955b2694c
“Roney <roneylima@zdzcode.com.br>
2025-01-31 09:34:01 -0400

    ajustes

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -136,7 +136,7 @@ export default defineNuxtConfig({
       host: process.env.MAIL_HOST ?? "",
       port: process.env.MAIL_PORT ?? "",
       user: process.env.MAIL_USER ?? "",
-      pass: process.env.MAIL_PASS ?? "",
+      pass: process.env.MAL_PASSWORD ?? "",
     },
     tokenSecret: process.env.JWT_SECRET ?? "",
     public: {
