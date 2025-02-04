3ce063a2a65f7684135a74e5283eb5232b80e8dc
interlig <128517162+interlig@users.noreply.github.com>
2025-02-04 16:49:19 -0400

    correção page

--- src/pages/teleconference/[id].vue
+++ src/pages/teleconference/[id].vue
@@ -2,7 +2,7 @@
   <CallQuery :solicitation="$single" />
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 definePageMeta({
   title: "Consulta online",
   description: "Telemedicina consulta online",
