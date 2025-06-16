99925c3b18b7edc4d51250ff5be9e25663f8f968
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2025-06-16 11:48:30 -0400

    ajuste imagem pacote

--- src/components/Packages/PackagesForm.vue
+++ src/components/Packages/PackagesForm.vue
@@ -64,8 +64,7 @@ const loading = ref(false);
 const model = ref({
   publicId: "",
   name: "",
-  urlImage:
-    "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  urlImage: "@/assets/images/package.avif",
   description: "",
   value: "",
   status: "",
@@ -131,8 +130,7 @@ const clearModel = () => {
   model.value = {
     publicId: "",
     name: "",
-    urlImage:
-      "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    urlImage: "@/assets/images/package.avif",
     description: "",
     value: "",
     status: "",
--- src/components/Service/ServicePackageCard.vue
+++ src/components/Service/ServicePackageCard.vue
@@ -17,11 +17,7 @@
       ></v-progress-linear>
     </template>
 
-    <v-img
-      height="250"
-      src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      cover
-    ></v-img>
+    <v-img height="250" src="@/assets/images/package.avif" cover></v-img>
 
     <v-card-item>
       <v-card-title>
