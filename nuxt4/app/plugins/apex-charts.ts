97223f65b2448ebde8a196bb711fe02a0d2ff81f
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2025-07-28 13:25:47 -0400

    correção plugin

--- nuxt4/app/plugins/apex-charts.ts
+++ nuxt4/app/plugins/apex-charts.ts
@@ -1,5 +1,5 @@
 import VueApexCharts from "vue3-apexcharts";
 
 export default defineNuxtPlugin((app) => {
-  app.vueApp.use(VueApexCharts);
+  app.vueApp.component("ApexChart", VueApexCharts);
 });
