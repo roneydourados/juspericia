3cac4d010fa2308d08045015fb4ab04710eed3fc
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2025-08-26 13:36:59 -0400

    remover zoom

--- nuxt4/nuxt.config.ts
+++ nuxt4/nuxt.config.ts
@@ -34,6 +34,13 @@ export default defineNuxtConfig({
     head: {
       title: "Juspericia",
       titleTemplate: "Juspericia",
+      meta: [
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        },
+      ],
       link: [
         {
           rel: "preconnect",
