6246a47eb358c5062540e608caeb557097ac1742
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2025-08-27 10:13:31 -0400

    detalhes solicitacao

--- nuxt4/app/components/Solicitation/SolicitationDetailsOffice.vue
+++ nuxt4/app/components/Solicitation/SolicitationDetailsOffice.vue
@@ -1,43 +1,49 @@
 <template>
   <div>
-    <div class="font-weight-bold mb-4 mt-4" style="font-size: 1.2rem">
+    <div
+      class="font-weight-bold text-primary mb-4 mt-4"
+      :style="`${mobile ? 'font-size: 0.8rem' : 'font-size: 1.2rem'}`"
+    >
       Dados do escritório
     </div>
-    <v-row dense>
+    <v-row dense class="text-primary">
       <v-col cols="12">
         <InfoLabel
           title="Email"
-          font-size="1"
+          :font-size="mobile ? '0.8' : '1'"
+          :font-size-content="mobile ? '0.8' : '1'"
           :content="$single?.Patient?.User?.email ?? 'Não informado'"
         />
       </v-col>
       <v-col cols="12" lg="8">
         <InfoLabel
           title="Nome do advogado"
-          font-size="1"
+          :font-size="mobile ? '0.8' : '1'"
+          :font-size-content="mobile ? '0.8' : '1'"
           :content="$single?.Patient?.User?.name ?? 'Não informado'"
         />
       </v-col>
       <v-col cols="12" lg="2">
         <InfoLabel
           title="OAB"
-          font-size="1"
+          :font-size="mobile ? '0.8' : '1'"
+          :font-size-content="mobile ? '0.8' : '1'"
           :content="$single?.Patient?.User?.oab ?? 'NNNNN'"
         />
       </v-col>
       <v-col cols="12" lg="2">
         <InfoLabel
           title="OAB/UF"
-          font-size="1"
+          :font-size="mobile ? '0.8' : '1'"
+          :font-size-content="mobile ? '0.8' : '1'"
           :content="$single?.Patient?.User?.oabUf ?? 'NN'"
         />
       </v-col>
-    </v-row>
-    <v-row dense>
       <v-col cols="12" lg="6">
         <InfoLabel
           title="CNPJ"
-          font-size="1"
+          :font-size="mobile ? '0.8' : '1'"
+          :font-size-content="mobile ? '0.8' : '1'"
           :content="
             $single?.Patient?.User?.cpfCnpj
               ? formatCPFOrCNPJ($single?.Patient?.User?.cpfCnpj ?? '')
@@ -48,7 +54,8 @@
       <v-col cols="12" lg="6">
         <InfoLabel
           title="Telefone"
-          font-size="1"
+          :font-size="mobile ? '0.8' : '1'"
+          :font-size-content="mobile ? '0.8' : '1'"
           :content="
             $single?.Patient?.User?.phone
               ? formatTelephoneNumber($single?.Patient?.User?.phone ?? '')
@@ -61,6 +68,9 @@
 </template>
 
 <script setup lang="ts">
+import { useDisplay } from "vuetify";
+
+const { mobile } = useDisplay();
 const { formatTelephoneNumber, formatCPFOrCNPJ } = useUtils();
 const storeConsultation = useSolicitationConsultationStore();
 
