4a40a80cb1245e245f38c250b2c01ac794978104
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2025-09-23 17:14:53 -0400

    ajuste largura do layout

--- nuxt4/app/layouts/default.vue
+++ nuxt4/app/layouts/default.vue
@@ -142,7 +142,7 @@
         :class="mobile ? 'w-100 px-2' : 'w-100 pa-4'"
         style="
           background-color: rgb(var(--v-theme-background)) !important;
-          max-width: 96.68rem;
+          max-width: 95%;
         "
       >
         <slot />
