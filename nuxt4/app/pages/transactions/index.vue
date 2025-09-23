3e17ee9c1ece1c03e61d73f7047ce8fea9bf49b7
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2025-09-23 16:56:52 -0400

    entrar filtrando as vendas do mes

--- nuxt4/app/pages/transactions/index.vue
+++ nuxt4/app/pages/transactions/index.vue
@@ -2,4 +2,15 @@
   <TransactionsTable />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import dayjs from "dayjs";
+
+const transactionsStore = useTransactionsStore();
+
+onMounted(async () => {
+  await transactionsStore.getTransactions({
+    initialDate: dayjs().startOf("month").format("YYYY-MM-DD"),
+    finalDate: dayjs().endOf("month").format("YYYY-MM-DD"),
+  });
+});
+</script>
