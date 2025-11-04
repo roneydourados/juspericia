5de0332044eb76c6aa416cdb1f5eb85304b4ec16
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2025-11-04 10:42:31 -0400

    ajuste

--- nuxt4/app/pages/transactions/index.vue
+++ nuxt4/app/pages/transactions/index.vue
@@ -9,7 +9,7 @@ const transactionsStore = useTransactionsStore();
 
 onMounted(async () => {
   await transactionsStore.getTransactions({
-    initialDate: dayjs().subtract(1, "month").format("YYYY-MM-DD"),
+    initialDate: dayjs().startOf("month").format("YYYY-MM-DD"),
     finalDate: dayjs().endOf("month").format("YYYY-MM-DD"),
     status: "CONFIRMED",
   });
